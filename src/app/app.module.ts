57a758e427e9eabafea6222f73595fdb0640800f
Henrique <rickfernandez03@gmail.com>
2023-06-28 22:42:02 -0300

    feat: home desktop completed. Just need game comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LayoutComponent } from './pages/layout/layout.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { GameComponent } from './components/game/game.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FooterComponent } from './components/footer/footer.component';
     HeaderComponent,
     HomeComponent,
     LayoutComponent,
-    FooterComponent
+    FooterComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
