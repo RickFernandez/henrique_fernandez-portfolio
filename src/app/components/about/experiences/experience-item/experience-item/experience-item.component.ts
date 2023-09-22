62f775585692368b10525878cbc684dc3e7411e9
Henrique <rickfernandez03@gmail.com>
2023-09-22 17:59:44 -0300

    feat: adding experience-item component

---
+++ src/app/components/about/experiences/experience-item/experience-item/experience-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experience-item',
+  templateUrl: './experience-item.component.html',
+  styleUrls: ['./experience-item.component.scss']
+})
+export class ExperienceItemComponent {
+
+}
