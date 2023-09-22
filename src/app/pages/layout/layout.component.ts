679c23e9e454eeed12e95117b15710297f3418d6
Henrique <rickfernandez03@gmail.com>
2023-09-22 17:51:56 -0300

    feat: adding layout component

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class LayoutComponent {
 
+  onSideNav: boolean = false;
+  sideType!: string;
+
+  constructor(private location: Location) {}
+
+  ngOnInit(): void {
+    this.location.onUrlChange((url: string, state: unknown) => {
+      url !== '/' && url !== '/home' ? this.onSideNav = true : this.onSideNav = false;
+      if (url.includes('/about-me')) {
+        this.sideType = '/about-me'
+      }
+      if (url.includes('/projects')) {
+        this.sideType = '/projects'
+      }
+    });
+  }
 }
