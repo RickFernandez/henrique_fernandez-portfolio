464c2d04a65c2acc36e30df42ea017f3c7f50625
Henrique Fernandez <rickfernandez03@gmail.com>
2025-02-19 23:40:43 -0300

    feat: opening dialog when navigation is bloacked.

--- src/app/components/core/header/header.component.ts
+++ src/app/components/core/header/header.component.ts
@@ -1,5 +1,8 @@
+import { DialogCloseConfig } from './../../../shared/components/global-dialog/global-dialog.model';
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
+import { DialogData, DialogSizeConfig } from 'src/app/shared/components/global-dialog/global-dialog.model';
+import { GlobalDialogService } from 'src/app/shared/components/global-dialog/global-dialog.service';
 
 @Component({
   selector: 'app-header',
@@ -10,14 +13,34 @@ export class HeaderComponent {
 
   menuOpened: boolean = false;
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private _authService: AuthService,
+    private _dialogService: GlobalDialogService
+  ) {}
 
   onOpenMenu() {
     this.menuOpened = !this.menuOpened;
   }
 
   onNavigation(): void {
-    if (this.authService.navigationIsAllow) return
-      alert('Ops... Navegação bloqueada. Vença de mim no jogo para liberá-la. 🤓');
+    if (this._authService.navigationIsAllow) return;
+
+    const dialogData: DialogData = {
+      title: 'Navegação bloqueada',
+      content: 'Se quer descobrir mais sobre mim, me vença no jokenpô! 🤓 \n Ou então, arregue e pule o jogo clicando no botão "Pular".',
+      hasCloseButton: true
+    }
+
+    const dialogSizeConfig: DialogSizeConfig = {
+      width: '40%',
+      height: '40%'
+    }
+
+    const DialogCloseConfig: DialogCloseConfig = {
+      isAutoClose: true,
+      autoCloseDelay: 10000
+    }
+
+    this._dialogService.open(dialogData, dialogSizeConfig, DialogCloseConfig);
   }
 }
